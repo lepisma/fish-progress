4e1011977dba40413de978f5a3233b698c2becec
Abhinav Tushar <abhinav@lepisma.xyz>
2020-05-17 09:57:12 +0530

    fix info string when number of chars are same in total and current

--- print_progress.fish
+++ print_progress.fish
@@ -1,8 +1,16 @@
 set -g progress_version 0.1.0
 
 function __progress_build_info -a total current -d "Build string showing completion info"
-    set -l done_str (string repeat -n (math (string length $total) - (string length $current)) " ")" "$current
-    echo ":: "$done_str" / "$total" "
+    set -l char_diff (math (string length $total) - (string length $current))
+
+    set -l padding
+    if test $char_diff -gt 0
+        set padding (string repeat -n $char_diff " ")" "
+    else
+        set padding " "
+    end
+
+    echo ":: "$padding" "$current" / "$total" "
 end
 
 function print_progress -a total current -d "Print one state of progress bar"
